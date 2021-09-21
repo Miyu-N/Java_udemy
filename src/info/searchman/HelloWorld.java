3105bd6b87936efafae6a4c594ad0196aa21d916
Miyu-N <myu.star1217@gmail.com>
2021-09-21 17:22:22 +0900

    eclipse使い方、変数

---
+++ src/info/searchman/HelloWorld.java
@@ -0,0 +1,13 @@
+package info.searchman;
+
+public class HelloWorld {
+	public static void main(String args[]) {
+		String x1 = "10";
+		int y1;
+		y1 = Integer.parseInt(x1);
+		String x2 = "5";
+		int y2;
+		y2 = Integer.parseInt(x2);
+		System.out.println(y1+y2);
+	}
+}
