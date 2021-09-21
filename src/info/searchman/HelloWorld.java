11a808104a7d9b93c3a51954136fdc3e292de700
Miyu-N <myu.star1217@gmail.com>
2021-09-21 18:53:02 +0900

    繰り返し処理 for, break

--- src/info/searchman/HelloWorld.java
+++ src/info/searchman/HelloWorld.java
@@ -2,16 +2,12 @@ package info.searchman;
 
 public class HelloWorld {
 	public static void main(String args[]) {
-		int umare;
-		umare = 1980;
-		if (umare < 1912) {
-			System.out.println("明治生まれです");
-		} else if (umare < 1927) {
-			System.out.println("大正生まれです");
-		} else if (umare < 1989) {
-			System.out.println("昭和生まれです");
-		} else {
-			System.out.println("平成生まれです");
+		int sum = 0;
+		int i; //iをforの外で定義する
+		for (i = 1; i <= 10; i ++) {
+		sum += i;
+		if( sum > 30)break;
 		}
+		System.out.println("30を超えたのは？：" + i);
 	}
 }
